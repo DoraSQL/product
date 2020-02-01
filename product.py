b16d3dd5ec7530851b018b1e9e02a3e26f717496
Dora W <shareff69@gmail.com>
2020-02-01 16:18:30 +0800

    basic funtion

---
+++ product.py
@@ -0,0 +1,13 @@
+
+product =[]
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':  #quit
+        break
+    price = input('請輸入商品價格: ')
+    # p=[]
+    # p.append(name)
+    # p.append(price)
+    # product.append(p)
+    product.append([name,price])
+print(product)
