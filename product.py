f80b4195a38a621f01065664f593d85e70729bde
Dora W <shareff69@gmail.com>
2020-02-03 17:38:13 +0800

    change to function

--- product.py
+++ product.py
@@ -1,40 +1,57 @@
 
 import os #operating ysytem
 
-product =[]
-if os.path.isfile('products.csv'): #檢查檔案在不在
-    print('yeah! 找到檔案了!')
-    #讀取檔案
-    with open('products.csv', 'r', encoding='utf-8') as f:
-        for line in f:
-            if '商品,價格' in line:
-                continue
-            name, price = line.strip().split(',')
-            product.append([name,price])
-    print(product)
-else:
-    print('找不到檔案... 哭哭 >w<')
+#讀取檔案
+def read_file(filename): 
+    product =[]
+    if os.path.isfile(filename): #檢查檔案在不在
+        print('yeah! 找到檔案了!')
+
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if '商品,價格' in line:
+                    continue
+                name, price = line.strip().split(',')
+                product.append([name,price])
+        print(product)
+    else:
+        print('找不到檔案... 哭哭 >w<')
+    return product
+
 
 #讓user輸入
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':  #quit
-        break
-    price = input('請輸入商品價格: ')
-    # p=[]
-    # p.append(name)
-    # p.append(price)
-    # product.append(p)
-    price = int(price) 
-    product.append([name,price])
-print(product)
+def user_input(product):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':  #quit
+            break
+        price = input('請輸入商品價格: ')
+        # p=[]
+        # p.append(name)
+        # p.append(price)
+        # product.append(p)
+        price = int(price) 
+        product.append([name,price])
+    print(product)
+    return product 
 
 #印出所有購買紀錄
-for p in product:
-    print(p[0], '的價格是', p[1])
+def print_products(product):
+    for p in product:
+        print(p[0], '的價格是', p[1])
+
 
 #寫入檔案
-with open('products.csv', 'w' , encoding='utf-8') as f:
-    f.write('商品,價格\n')
-    for p  in product:
-    	f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, product):
+    with open(filename, 'w' , encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p  in product:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+
+product = read_file('products.csv')
+product = user_input(product)
+print_products(product)
+write_file('products.csv', product )
+
